77c2588a6a6906a21958c692c0e7408fedf69eaa
workani <tomdanil68@gmail.com>
2024-09-14 22:55:14 +0200

    init

---
+++ GetInput/Class1.cs
@@ -0,0 +1,38 @@
+﻿namespace GetInput;
+
+public class Get
+{
+    /* Get a number from user and handle all possible exceptions */
+    public int GetInt(string prompt)
+    {
+        return 0;
+    }
+
+    public static float GetFloat(string prompt)
+    {
+        
+    }
+
+    public static double GetDouble(string prompt)
+    {
+        
+    }
+
+    public static decimal GetDecimal(string prompt)
+    {
+        
+    }
+
+    public static int GetInRangeInt(int lowerBound, int upperBound, string prompt)
+    {
+        
+    }
+    
+    /* get string */
+
+    public static string GetString(string promt)
+    {
+        
+    }
+    
+}
