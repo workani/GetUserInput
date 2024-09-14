3737454d49e68090df7d611c0cffb84a22a796eb
workani <tomdanil68@gmail.com>
2024-09-14 23:35:38 +0200

    Implemnt all functions. Removed functions for getting float and decimal. Added functions for getting path to folder and file

--- GetInput/Class1.cs
+++ GetInput/Class1.cs
@@ -1,38 +1,153 @@
-﻿namespace GetInput;
+﻿using System;
+namespace GetInput;
 
 public class Get
 {
     /* Get a number from user and handle all possible exceptions */
     public int GetInt(string prompt)
     {
-        return 0;
+        while (true)
+        {
+            Console.WriteLine(prompt);
+            
+            try
+            {
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\u001b[31mPlease, type a valid number.\u001b[0m");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"\u001b[31mProvided number is too large or to small.\u001b[0m");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\u001b[31mSomething went wrong. Please try typing your input again!\u001b[0m");
+            }
+        }
     }
-
-    public static float GetFloat(string prompt)
-    {
-        
-    }
-
+    
     public static double GetDouble(string prompt)
     {
-        
+        while (true)
+        {
+            Console.WriteLine(prompt);
+            
+            try
+            {
+                return Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\u001b[31mPlease, type a valid number.\u001b[0m");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"\u001b[31mProvided number is too large or to small.\u001b[0m");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\u001b[31mSomething went wrong. Please try typing your input again!\u001b[0m");
+            }
+        }
     }
 
-    public static decimal GetDecimal(string prompt)
-    {
-        
-    }
+   
 
     public static int GetInRangeInt(int lowerBound, int upperBound, string prompt)
     {
-        
+        while (true)
+        {
+            Console.Write(prompt);
+
+            try
+            {
+                int result = Convert.ToInt32(Console.ReadLine());
+
+                if (result >= lowerBound && result <= upperBound)
+                {
+                    Console.Clear();
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine($"\u001b[31mPlease, type a number in range {lowerBound}-{upperBound}.\u001b[0m");
+                }
+                    
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\u001b[31mPlease, type a valid number.\u001b[0m");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"\u001b[31mProvided number is too large or to small. Please, type a number in range {lowerBound}-{upperBound}.\u001b[0m");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\u001b[31mSomething went wrong. Please try typing your input again!\u001b[0m");
+            }
+        }
     }
     
-    /* get string */
+    /* Get string */
 
-    public static string GetString(string promt)
+    public static string GetString(string prompt)
     {
-        
+        while (true)
+        {
+            Console.Write(prompt);
+            string userInput = Console.ReadLine();
+            
+            if(!string.IsNullOrWhiteSpace(userInput))
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("\u001b[31mProvided string is empty, please type it again!\u001b[0m");
+            }
+        }
+      
     }
+    
+    /* Get the path to a file or a directory and check if it exists */
+
+    public static string GetFilePath(string prompt)
+    {
+        while (true)
+        {
+            string path = GetString(prompt);
+            
+            if (File.Exists(path))
+            {
+                return path;
+            }
+            else
+            {
+                Console.WriteLine("\u001b[31m Provided path to the file is incorrect. Please, try again!\u001b[0m");
+            }
+        }
+    }
+
+    public static string GetDirectory(string prompt)
+    {
+        while (true)
+        {
+            string path = GetString(prompt);
+            
+            if (Directory.Exists(path))
+            {
+                return path;
+            }
+            else
+            {
+                Console.WriteLine("\u001b[31mThe provided folder doesn't exist. Please provide the correct path to your folder!\u001b[0m");
+            }
+        }
+    }
+    
     
 }
